d9cfe7e53f45efd887b4cdc5b5c324a1c15a3c95
Marcin Jusiak <46385598+marcinj94@users.noreply.github.com>
2024-02-21 23:06:21 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+// eslint-disable-next-line object-curly-newline
+const nextConfig = {
+  experimental: {
+    forceSwcTransforms: true,
+  },
+};
+
+module.exports = nextConfig;
