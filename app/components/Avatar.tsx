a5c5ab5a510a9e1dbbd16629cec6455d169576c5
Marcin Jusiak <mjusiak@efigence.com>
2024-02-24 22:55:03 +0100

    feat(tailwindcss): configure tailwindCSS | Create basic Avatar component

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,17 @@
+export function Avatar() {
+  return (
+    // <div className="p-4 ">
+    //   <div className="h-36 w-36 rounded-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
+    //     <div className="h-full w-full bg-gray-800">
+    //       <h1 className="text-2xl font-black text-white">MJ</h1>
+    //     </div>
+    //   </div>
+    // </div>
+    // <div className="rounded-full w-24 h-24 mx-auto  mt-10 bg-gradient-to-r p-[6px] from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]">
+    <div className="rounded-full w-24 h-24 mx-auto  mt-10 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-1">
+      <div className="flex justify-center items-center flex-col h-full bg-gray-800 text-white rounded-full p-4">
+        MJ
+      </div>
+    </div>
+  );
+}
