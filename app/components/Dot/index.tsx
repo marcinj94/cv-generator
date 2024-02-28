ed1a8e45f0d10156a0de6ef7c8fd0e3cf60ec59e
Marcin Jusiak <mjusiak@efigence.com>
2024-02-28 17:39:39 +0100

    feat(components): create Dot & Education components

---
+++ app/components/Dot/index.tsx
@@ -0,0 +1,16 @@
+import clsx from 'clsx';
+
+interface DotProps {
+  isAbsolute?: boolean;
+}
+
+export const Dot: React.FC<DotProps> = ({ isAbsolute }) => (
+  <div
+    className={clsx(
+      'w-2 h-2 rounded-full top-1/2 -left-4 -translate-y-1/2  bg-gradient-to-r from-green-500 to-blue-500',
+      {
+        absolute: isAbsolute,
+      },
+    )}
+  />
+);
--- app/components/Education/Education.stories.tsx
+++ app/components/Education/Education.stories.tsx
@@ -6,12 +6,8 @@ const meta: Meta<typeof Education> = {
   // 👇 Enables auto-generated documentation for the component story
   tags: ['autodocs'],
   argTypes: {
-    firstProp: {
-      control: 'text',
-    },
-    secondProp: {
-      control: 'radio',
-      options: ['option1', 'option2'],
+    educations: {
+      defaultValue: [],
     },
   },
 };
@@ -19,16 +15,29 @@ const meta: Meta<typeof Education> = {
 export default meta;
 type Story = StoryObj<typeof Education>;
 
-export const FirstVersion: Story = {
+export const ListOfUniversities: Story = {
   args: {
-    firstProp: 'first-prop-v1',
-    secondProp: 'option1',
-  },
-};
-
-export const SecondVersion: Story = {
-  args: {
-    firstProp: 'first-prop-v2',
-    secondProp: 'option1',
+    educations: [
+      {
+        school: 'Josip Juraj Strossmayer University of Osijek',
+        years: '10/2017 - 02/2018',
+        level: 'Master of Engineering | Erasmus+',
+      },
+      {
+        school: 'Lublin University of Technologi',
+        years: '2017 - 2018',
+        level: 'Master of Engineering in Computer Science | spec. Internet Application',
+      },
+      {
+        school: 'Suleyman Demirel University',
+        years: '02/2016 - 06/2016',
+        level: 'Bachelor of Engineering | Erasmus+',
+      },
+      {
+        school: 'Lublin University of Technologi',
+        years: '2013 - 2017',
+        level: 'Bachelor of Engineering in Computer Science',
+      },
+    ],
   },
 };
