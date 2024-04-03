d0ba76e06d570f125fcd1683dbb6733d45db98e8
Marcin Jusiak <marcin.jusiak@gmail.com>
2024-04-03 16:03:10 +0200

    refactor(component): handle dark-mode on Input component

--- app/components/Input/index.tsx
+++ app/components/Input/index.tsx
@@ -38,7 +38,7 @@ export const Input: React.FC<InputProps> = ({
     <div className="relative my-2">
       <input
         className={clsx(
-          'peer shadow appearance-none border-2 rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-w-36 focus:border-blue-600 transition valid:border-blue-600',
+          'peer shadow appearance-none border-2 rounded w-full py-2 px-3 bg-white dark:bg-gray-800 dark:text-white text-gray-700 leading-tight focus:outline-none focus:shadow-outline min-w-36 focus:border-blue-600 transition valid:border-blue-600',
           {
             'border-red-600': error,
             'pr-10': togglePassword,
@@ -51,14 +51,21 @@ export const Input: React.FC<InputProps> = ({
         value={value}
         required
       />
-      {Icon && <Icon className="absolute top-1/2 left-3 translate-y-[-50%]" size={ICON_SIZE} />}
+      {Icon && (
+        <Icon
+          className="absolute top-1/2 left-3 translate-y-[-50%]"
+          size={ICON_SIZE}
+          color={error ? '#dc2626' : '#2563eb'}
+        />
+      )}
       <label
         className={clsx(
-          'transition-all cursor-text block absolute top-1/2 left-3 px-1 py-0 bg-white translate-y-[-50%]  text-sm peer-focus:font-bold peer-focus:text-blue-600 peer-focus:top-0 peer-focus:left-6 peer-valid:font-bold peer-valid:text-blue-600 peer-valid:top-0 peer-valid:left-6',
+          'transition-all cursor-text block absolute top-1/2 px-1 py-0 bg-white dark:bg-gray-800 translate-y-[-50%]  text-sm peer-focus:font-bold peer-focus:text-blue-600 peer-focus:top-0 peer-focus:left-6 peer-valid:font-bold peer-valid:text-blue-600 peer-valid:top-0 peer-valid:left-6',
           {
             'peer-invalid:font-bold peer-invalid:top-0 peer-invalid:left-6 peer-invalid:text-red-600':
               error,
             'left-10': Icon,
+            'left-3': !Icon,
           },
         )}
         htmlFor={id}
